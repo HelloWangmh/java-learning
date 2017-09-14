80f77e4883bd02f1bdae2e4dca786df641dc6d47
wangminghui <wangminghui@zyxtravel.com>
2017-09-14 12:16:30 +0800

    引入guava

---
+++ src/main/java/wang/mh/guava/CollectionDemo.java
@@ -0,0 +1,19 @@
+package wang.mh.guava;
+
+import com.google.common.collect.Collections2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CollectionDemo {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        Collection<Integer> filter = Collections2.filter(list, input -> input == 1);
+        filter.forEach(integer -> System.out.println(integer));
+        list.forEach(integer -> System.out.println(integer));
+    }
+}
