3f5c15a4eaa10e4d68d01f00581c0559bcec05d3
wangminghui <wangminghui@zyxtravel.com>
2017-09-20 10:34:03 +0800

    bug

--- src/main/java/wang/mh/guava/CollectionDemo.java
+++ src/main/java/wang/mh/guava/CollectionDemo.java
@@ -12,7 +12,7 @@ public class CollectionDemo {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
-        Collection<Integer> filter = Collections2.filter(list, input -> input == 1);
+        Collection<Integer> filter = Collections2.filter(list, input -> input !=null && input == 1);
         filter.forEach(integer -> System.out.println(integer));
         list.forEach(integer -> System.out.println(integer));
     }
