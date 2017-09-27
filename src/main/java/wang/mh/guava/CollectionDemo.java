7d0103beee0fee2428aecc3abfca2944814a84d4
wangminghui <wangminghui@zyxtravel.com>
2017-09-27 17:29:20 +0800

    注解的学习

--- src/main/java/wang/mh/guava/CollectionDemo.java
+++ src/main/java/wang/mh/guava/CollectionDemo.java
@@ -1,19 +1,24 @@
 package wang.mh.guava;
 
 import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import wang.mh.common.Father;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionDemo {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        Collection<Integer> filter = Collections2.filter(list, input -> input !=null && input == 1);
-        filter.forEach(integer -> System.out.println(integer));
-        list.forEach(i -> System.out.println(i));
+        ArrayList<String> list = Lists.newArrayList();
+        for(int i =0 ;i<10000;i++){
+            list.add("test");
+        }
+        List<List<String>> partition = Lists.partition(list, 2000);
+        partition.forEach(l -> {
+            System.out.println(l.size());
+        });
     }
 }
