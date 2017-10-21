886e8ba048b9fc6ba74a3d8d93150ef9763415f5
wmh <hellowangmh@163.com>
2017-10-22 00:41:11 +0800

    DataInput/DataOutput learn

---
+++ src/main/java/wang/mh/base/io/DataInputOutputDemo.java
@@ -0,0 +1,91 @@
+package wang.mh.base.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.*;
+
+/**
+ * DataOutput,DataInput   可以将数据以字节读取,写入
+ * 子类:DataOutputStream,DataInputStream
+ * RandomAccessFile实现了DataOutput和DataInput    可以根据seek()方法指定读取/写入的位置
+ *
+ */
+public class DataInputOutputDemo {
+
+    public static final String FILE_PATHH = "src/main/resources/io/user.dat";
+
+    public static void main(String[] args) throws Exception {
+
+        User[] users = new User[]{new User("wmh", 18, 888.0),
+                                  new User("wang mh", 8, 8888.0),
+                                  new User("hello,world!", 28, 88888.0)};
+        //write
+        try (DataOutputStream out = new DataOutputStream(new FileOutputStream(FILE_PATHH))){
+            for (User user : users) {
+                DataIo.writeFixedString(user.getName(), User.NAME_SIZE, out);
+                out.writeInt(user.getAge());
+                out.writeDouble(user.getMoney());
+            }
+        }
+
+        //read
+        try (RandomAccessFile in = new RandomAccessFile(FILE_PATHH, "r")){
+            int num = (int)in.length() / (User.TOTAL_SIZE);
+            for (int i = 0; i < num; i++) {
+                String name = DataIo.readFixedString(User.NAME_SIZE, in);
+                int age = in.readInt();
+                double money = in.readDouble();
+                User u = new User(name, age, money);
+                System.out.println(u);
+            }
+        }
+
+    }
+}
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+class User{
+
+    private String name;
+    private Integer age;
+    private double money;
+
+    public static final int NAME_SIZE = 20;
+    //num of byte
+    public static final int TOTAL_SIZE = 2 * NAME_SIZE + 4 + 8;
+
+}
+
+class DataIo{
+
+    public static void writeFixedString(String s, int size, DataOutput out)
+            throws IOException {
+        for (int i = 0; i < size; i++) {
+            char ch = 0;
+            if (i < s.length()) ch = s.charAt(i);
+            out.writeChar(ch);
+        }
+    }
+
+    public static String readFixedString(int size, DataInput in) throws IOException {
+        StringBuilder s = new StringBuilder(size);
+        int i = 0;
+        boolean more = true;
+        while (more && i < size) {
+            char c = in.readChar();
+            i++;
+            if (c == 0) more = false;
+            else s.append(c);
+        }
+        //a char  is 2 byte
+        in.skipBytes(2 * (size - i));
+        return s.toString();
+    }
+}
---
+++ src/main/java/wang/mh/base/io/IODemo.java
@@ -0,0 +1,14 @@
+package wang.mh.base.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class IODemo {
+    public static byte b;
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
