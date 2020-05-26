ece20233ed4e64b9768fd40626d01e95615ea916
wmh <wangminghui@zyxtravel.com>
2020-05-26 18:33:12 +0800

    +AQSDemo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 /**
  * Created by 明辉 on 2017/7/30.
@@ -7,6 +7,11 @@
  */
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+
+    class AqsDemo extends AbstractQueuedSynchronizer {
 
     }
 }
---
+++ src/main/java/wang/mh/thread/demo/AQSDemo.java
@@ -0,0 +1,76 @@
+package wang.mh.thread.demo;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+public class AQSDemo {
+
+
+    private static int count = 0;
+    private static Mutex mutex = new Mutex();
+
+    public static void main(String[] args) throws Exception {
+        CountDownLatch latch = new CountDownLatch(100);
+
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 10000; j++) {
+                    increment();
+                }
+                latch.countDown();
+            }).start();
+        }
+        latch.await();
+        System.out.println("count : " + count);
+    }
+
+    private static void increment() {
+        mutex.lock();
+        count++;
+        mutex.unlock();
+    }
+
+
+
+    static class Mutex {
+
+        private Sync sync = new Sync();
+
+        public boolean lock() {
+            return sync.tryAcquire(1);
+        }
+
+        public boolean unlock() {
+            return sync.tryRelease(1);
+        }
+
+
+        private  class Sync extends AbstractQueuedSynchronizer {
+
+            //是否被占用
+            @Override
+            protected boolean isHeldExclusively() {
+                return getState() == 1;
+            }
+
+            @Override
+            protected boolean tryAcquire(int arg) {
+                if (compareAndSetState(0, 1)) {
+                    setExclusiveOwnerThread(Thread.currentThread());
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            protected boolean tryRelease(int arg) {
+                if (getState() == 0) {
+                    throw new IllegalMonitorStateException();
+                }
+                setExclusiveOwnerThread(null);
+                setState(0);
+                return true;
+            }
+        }
+    }
+}
